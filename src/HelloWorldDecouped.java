5fd08bf93273716a36c44786e4d9daa85998ccda
Sergey Stepygin <stepygin.sergey@gmail.com>
2018-10-27 22:32:21 +0300

    Сделан рефакторинг классов, для вывода сообщений

---
+++ src/HelloWorldDecouped.java
@@ -0,0 +1,10 @@
+public class HelloWorldDecouped {
+
+    public static void main(String[] args) {
+        MessageRenderer mr = new StandartOutMessageRenderer();
+        MessageProvider mp = new HelloWorldMessageProvider();
+        mr.setMessageProvider(mp);
+        mr.render();
+    }
+
+}
