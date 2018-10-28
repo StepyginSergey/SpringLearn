34ce20d2729ca63658473341241fac957aa855b2
Sergey Stepygin <stepygin.sergey@gmail.com>
2018-10-28 19:34:33 +0300

    Добавлен пример использования фабрики поддержки

---
+++ src/HelloWorldDecoupedWithFactory.java
@@ -0,0 +1,10 @@
+public class HelloWorldDecoupedWithFactory {
+
+    public static void main(String[] args) {
+        MessageRenderer mr = MessageSupportFactory.getInstance().getMessageRenderer();
+        MessageProvider mp = MessageSupportFactory.getInstance().getMessageProvider();
+        mr.setMessageProvider(mp);
+        mr.render();
+    }
+
+}
---
+++ src/MessageSupportFactory.java
@@ -0,0 +1,40 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class MessageSupportFactory {
+    private static MessageSupportFactory instance;
+
+    private Properties properties;
+    private static MessageRenderer renderer;
+    private static MessageProvider provider;
+
+    private MessageSupportFactory(){
+        properties = new Properties();
+        try{
+            properties.load(new FileInputStream("src/msf.properties"));
+
+            String rendererClass = properties.getProperty("renderer.class");
+            String providerClass = properties.getProperty("provider.class");
+
+            renderer = (MessageRenderer) Class.forName(rendererClass).newInstance();
+            provider = (MessageProvider) Class.forName(providerClass).newInstance();
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    static {
+        instance = new MessageSupportFactory();
+    }
+
+    public static MessageSupportFactory getInstance(){
+        return instance;
+    }
+    public static MessageRenderer getMessageRenderer(){
+        return renderer;
+    }
+    public static MessageProvider getMessageProvider(){
+        return provider;
+    }
+}
